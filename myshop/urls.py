0092c2024c7633abd4659a1a9d2966363d879664
Mintimir <kachaev.666@gmail.com>
2021-07-10 15:17:36 +0600

    added update, post, patch request to Category

--- myshop/urls.py
+++ myshop/urls.py
@@ -4,11 +4,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('categories/', CategoriesView.as_view(), name='products-list'),
+    path('categories/', CategoriesView.as_view(), name='categories-list'),
+    path('category_detail/<int:pk>/', CategoryDetailView.as_view(), name='categories-detail'),
     path('products/', ProductsListView.as_view(), name='products-list'),
     path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='products-detail'),
-    path('product_detail/<int:pk>/pictures', ProductPicturesListView.as_view(), name='products-pictures'),
-    path('product_detail/<int:pk>/pictures/<int:alt_pk>', ProductPictureDetailView.as_view(), name='products-pictures-detail'),
+    path('product_detail/<int:pk>/pictures/', ProductPicturesListView.as_view(), name='products-pictures'),
+    path('product_detail/<int:pk>/pictures/<int:alt_pk>/', ProductPictureDetailView.as_view(), name='products-pictures-detail'),
     path('comments_of_product/<int:pk>/', CommentsView.as_view(), name='comments-of-product'),
     path('cart/', CartView.as_view(), name='cart')
 ]
--- myshop/views.py
+++ myshop/views.py
@@ -47,27 +47,6 @@ class ProductDetailView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-
-
-class CategoriesView(APIView):
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-
-
-class CartView(APIView):
-    def get(self, request):
-        cart_items = CartItem.objects.filter(status=True)
-        serializer = CartSerializer({'products':cart_items})
-        return Response(serializer.data)
-
-class CommentsView(APIView):
-    def get(self, request, pk):
-        comments = Comment.objects.filter(comment_of_reply=None, product=pk)
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
-
 class ProductPicturesListView(APIView):
 
     def get(self, request, pk):
@@ -91,3 +70,50 @@ class ProductPictureDetailView(APIView):
         picture = ProductPicture.objects.get(id=alt_pk)
         picture.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoriesView(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)     
+
+class CategoryDetailView(APIView):
+    def get(self, request, pk):
+        category = get_object(Category, pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        category = get_object(Category, pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        category = get_object(Category, pk)
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CartView(APIView):
+    def get(self, request):
+        cart_items = CartItem.objects.filter(status=True)
+        serializer = CartSerializer({'products':cart_items})
+        return Response(serializer.data)
+
+class CommentsView(APIView):
+    def get(self, request, pk):
+        comments = Comment.objects.filter(comment_of_reply=None, product=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
